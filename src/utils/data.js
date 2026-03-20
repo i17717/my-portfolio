59e7fb6b7ce3cb8d2594b9955db09aa2cf5b9fb2
i17717 <ruaa.ima@gmail.com>
2026-03-20 15:20:44 +0200

    update resume link

--- src/utils/data.js
+++ src/utils/data.js
@@ -21,4 +21,4 @@ export const links = [
   }
 ];
 
-export const cvURL = "https://drive.google.com/file/d/1O_OpCw3Jya38iW46Kn-jtkehi4ZMOiXx/view?usp=sharing";
+export const cvURL = "https://drive.google.com/file/d/1m-vW18ndDZ1OIfoXvdYlqVlxu18Dneps/view?usp=sharing";
